da2b0930b282afd144c86b23d1bc51c89f75734b
Car0linaaS <112877280+Car0linaaS@users.noreply.github.com>
2025-05-20 00:17:15 +0200

    Add use of next's Image component

--- frontent-test/src/app/components/PostCard.tsx
+++ frontent-test/src/app/components/PostCard.tsx
@@ -3,6 +3,7 @@ import CardPicture from "./CardPicture";
 import CardExcerpt from "./CardExcerpt";
 import CardFooter from "./CardFooter";
 import { Post } from "../types/Post";
+import Image from "next/image";
 
 interface PostCardProps {
   post: Post;
@@ -12,10 +13,14 @@ export default function PostCard({ post }: PostCardProps) {
   return (
     <div className="flex flex-col justify-between bg-[#f0f5fd] outline-[#1974f9] outline-1 rounded-xl h-full">
       <CardTitle title={post.title.rendered}></CardTitle>
-      <CardPicture
-        src={post._embedded["wp:featuredmedia"]["0"].source_url}
-        alt={post.title.rendered}
-      ></CardPicture>
+      <div className="w-full aspect-[16/9] relative overflow-hidden">
+        <Image
+          src={post._embedded["wp:featuredmedia"]["0"].source_url}
+          alt={post.title.rendered}
+          fill
+          className="object-cover"
+        />
+      </div>
       <CardExcerpt excerpt={post.excerpt.rendered}></CardExcerpt>
       <CardFooter date={post.date} id={post.id}></CardFooter>
     </div>
