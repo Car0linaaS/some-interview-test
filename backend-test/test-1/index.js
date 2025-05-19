5b4f8a890516e181a7cd6e504a64a533c5dc23ab
Car0linaaS <112877280+Car0linaaS@users.noreply.github.com>
2025-05-19 22:11:17 +0200

    Add implementation for chunk function

--- backend-test/test-1/index.js
+++ backend-test/test-1/index.js
@@ -4,7 +4,32 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-  
+    const returnArr = [];
+
+    // Increase iterator index with step size 'size'
+    for (let i = 0; i < arr.length; i +=size){
+        // Extract a slice to create a subarray of length 'size'.
+        // Subarray will contain the remaining elements if less than 'size' elements remain 
+        subArr = arr.slice(i, i+size);
+        returnArr.push(subArr);
+    }
+
+    return returnArr;
 };
 
+// Test print
+let arr = [1,9,6,3,2];
+size = 3;
+console.log("Input array:", arr, "chunkSize:", size, "\nOutput:", chunk(arr, size), "\n");
 
+arr = [8,5,3,2,6]; 
+size = 6;
+console.log("Input array:", arr, "chunkSize:", size, "\nOutput:", chunk(arr, size), "\n");
+
+arr = [1,2,3,4,5];
+size = 1;
+console.log("Input array:", arr, "chunkSize:", size, "\nOutput:", chunk(arr, size), "\n");
+
+arr = [];
+size = 1;
+console.log("Input array:", arr, "chunkSize:", size, "\nOutput:", chunk(arr, size), "\n");
