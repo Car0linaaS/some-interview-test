8b265cfd1792f86f33e411a874a28d28bcd7518c
Car0linaaS <112877280+Car0linaaS@users.noreply.github.com>
2025-05-20 00:24:10 +0200

    Update card excerpt to use sanitized html component

--- frontent-test/src/app/components/CardExcerpt.tsx
+++ frontent-test/src/app/components/CardExcerpt.tsx
@@ -1,17 +1,16 @@
 import { chivo_mono } from "../ui/fonts";
+import SanitizedHtml from "@/app/components/SanitizedHtml";
 
 interface CardExcerptProps {
   excerpt: string;
 }
 
 export default function CardExcerpt({ excerpt }: CardExcerptProps) {
-  // Remove all html tags etc
-  const plainText = excerpt.replace(/<[^>]+>/g, "");
   return (
-    <div className={`${chivo_mono.className} m-4 mb-0`}>
-      <p className="line-clamp-2 text-xs md:text-sm sm:text-xs lg:text-base">
-        {plainText}
-      </p>
+    <div
+      className={`${chivo_mono.className} m-4 mb-0 line-clamp-2 text-xs md:text-sm sm:text-xs lg:text-base`}
+    >
+      <SanitizedHtml html={excerpt} />
     </div>
   );
 }
